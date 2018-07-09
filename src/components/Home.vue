e85ef8885883c63e9a657e4c44ae43a663b1d0c3
princel3lcS <princeboy__@hotmail.com>
2018-07-09 17:51:27 +0700

    update/form login

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,8 +1,31 @@
 <template>
   <div style="position: relative; z-index: 2; background-color: #000; height: 100vh;">
     <parallax>
-      <div class="foo" style="position: absolute; z-index: 3; color:#fff; font-size:40px;"> foo</div>
-      <div class="bar" style="position: absolute; z-index: 3; color:#fff; font-size:40px; top: 50px;"> Bar </div>
+      <div class="row">
+        <div class="col s2"/>
+        <div class="input-field col s8 load">
+          <input type="text" id="username" autocomplete="off">
+          <label for="username">USERNAME</label>
+        </div>
+        <div class="col s2"/>
+      </div>
+      <div class="row">
+        <div class="col s2"/>
+        <div class="input-field col s8 load">
+          <input type="password" id="password" autocomplete="off">
+          <label for="password">PASSWORD</label>
+        </div>
+        <div class="col s2"/>
+      </div>
+      <div class="row">
+        <div class="col s2"/>
+        <div class="col s2 load">
+          <button class="btn waves-effect waves-light" type="submit" name="action">Submit
+            <i class="material-icons right">send</i>
+          </button>
+        </div>
+        <div class="col s2"/>
+      </div>
     </parallax>
   </div>
 </template>
@@ -22,11 +45,16 @@ export default {
       duration: 400,
       delay: 200
     })
-    sr.reveal('.foo', { delay: 0, origin: 'top' }, 200)
-    sr.reveal('.bar', 200)
+    sr.reveal('.load', { delay: 0, origin: 'top' }, 200)
   },
   components: {
     Parallax
   }
 }
 </script>
+
+<style>
+#username, #password{
+  color:aliceblue;
+}
+</style>
