02a9ba529c8d78e45202a5f66f305154b350ebc1
princel3lcS <princeboy__@hotmail.com>
2018-07-09 15:39:39 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  beforeCreate () {
+    console.log('beforeCreate')
+  },
+  created () {
+    console.log('created')
+  },
+  beforeMount () {
+    console.log('beforeMount')
+  },
+  mounted () {
+    console.log('mounted')
+  },
+  beforeUpdate () {
+    console.log('beforeUpdate')
+  },
+  updated () {
+    console.log('updated')
+  },
+  beforeDestroy () {
+    console.log('beforeDestroy')
+  },
+  destroyed () {
+    console.log('destroyed')
+  }
+}
+</script>
---
+++ src/components/Home.vue
@@ -0,0 +1,37 @@
+<template>
+    <!-- <parallax-container>
+      <parallax-element class="background" :parallaxStrength="-10" :type="'depth'">
+        <h2>'depth'</h2>
+      </parallax-element>
+    </parallax-container> -->
+  <div style="position: relative; z-index: 2; background-color: #000;">
+    <parallax>
+      <div class="foo" style="position: absolute; z-index: 3; color:#fff; font-size:40px;"> foo</div>
+      <div class="bar" style="position: absolute; z-index: 3; color:#fff; font-size:40px; top: 50px;"> Bar </div>
+    </parallax>
+  </div>
+</template>
+
+<script>
+import Parallax from 'vue-parallaxy'
+import ScrollReveal from 'scrollreveal'
+export default {
+  name: 'Home',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  mounted () {
+    const sr = ScrollReveal({
+      duration: 400,
+      delay: 200
+    })
+    sr.reveal('.foo', { delay: 0, origin: 'top' }, 200)
+    sr.reveal('.bar', 200)
+  },
+  components: {
+    Parallax
+  }
+}
+</script>
